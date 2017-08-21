6ad477487293752c605257510305986fea9d520b
Kai <kai.schaefer@mni.thm.de>
2017-08-21 14:00:44 +0200

    Datenbankhandler um Funktion Hinzufügen eines Plans erweitert

--- src/datamodel/DatabaseHandler.java
+++ src/datamodel/DatabaseHandler.java
@@ -3,6 +3,8 @@ package datamodel;
 import java.io.File;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 /**
  * Created by Kai on 18.08.2017.
@@ -34,6 +36,21 @@ public class DatabaseHandler {
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    public boolean addPlan(int dis, String content){
+        String query = "INSERT INTO Plan (Distanz, Inhalt)  VALUES (?, ?);";
+        try{
+            PreparedStatement stmt = connection.prepareStatement(query);
+
+            stmt.setInt(1, dis);
+            stmt.setString(2, content);
+            stmt.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false; 
+        }
+        return true;
     }
 
 }
