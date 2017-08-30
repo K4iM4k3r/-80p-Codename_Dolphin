14db9e0ef076c0a66eb7207f92923de5eb8c822d
Kai <kai.schaefer@mni.thm.de>
2017-08-30 18:50:42 +0200

    Klasse zum Handling der Distanzeingabe

---
+++ src/datamodel/Distance.java
@@ -0,0 +1,23 @@
+package datamodel;
+
+/**
+ * Created by Kai on 30.08.2017.
+ * part of 80p - Codename Dolphin
+ */
+public enum Distance {
+    LOW(" distanz <  2000"),
+    SHORT(" distance >= 2000 AND distance < 3000"),
+    MEDIUM(" distanz >= 3000 AND distanz < 4000"),
+    LONG(" distanz >= 4000");
+
+    private final String clause;
+
+    Distance(String clause){
+        this.clause = clause;
+    }
+
+    public String getClause() {
+        return clause;
+    }
+
+}
